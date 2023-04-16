a659b1260a37763a1178b067a038e493c0bc362e
androiddevnotes <awesomedevnotes@gmail.com>
2023-04-16 12:11:36 +0100

    Added logging and ignored files.

---
+++ app/src/main/java/com/example/bettertimberlogsandroid/ClickableLineNumberDebugTree.kt
@@ -0,0 +1,34 @@
+package com.example.bettertimberlogsandroid
+
+import timber.log.Timber
+
+class ClickableLineNumberDebugTree : Timber.DebugTree() {
+
+    override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        findLogCallStackTraceElement()?.let { element ->
+            val lineNumberInfo = "(${element.fileName}:${element.lineNumber})"
+            val updatedMessage = "$lineNumberInfo: $message"
+            super.log(priority, tag, updatedMessage, t)
+        } ?: run {
+            super.log(priority, tag, message, t)
+        }
+    }
+
+    override fun createStackElementTag(element: StackTraceElement): String? {
+        return element.className
+    }
+
+    private fun findLogCallStackTraceElement(): StackTraceElement? {
+        val stackTrace = Throwable().stackTrace
+        var foundDebugTree = false
+
+        return stackTrace.firstOrNull { element ->
+            if (element.className.contains("ClickableLineNumberDebugTree")) {
+                foundDebugTree = true
+                false
+            } else {
+                foundDebugTree && !element.className.contains("Timber")
+            }
+        }
+    }
+}
